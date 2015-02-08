ca64671e58c2a111ecc370f9e91457622a3ffbd3
Sebastian Ruml <sebastian.ruml@gmail.com>
2015-02-08 18:35:19 +0100

    current status

---
+++ roles/gogs/templates/create_admin_user.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SALT=`head -c 10 /dev/random | base64 | cut -c-10`
+
+PASSWORD=$(
+python3 - <<EOF
+import hashlib, binascii
+dk = binascii.hexlify(hashlib.pbkdf2_hmac('sha256', b'er5gi8theik1Uha', b'${SALT}', 10000, 50'))
+print(str(dk, "ascii"))
+EOF
+)
+
+mysql -h localhost -u{{gogs_db_user}} -p{{gogs_db_password}} --protocol tcp {{gogs_db_name}} <<EOF
+INSERT INTO user (lower_name, name, email, passwd, salt, is_active, is_admin)
+SELECT * FROM (SELECT '{{gogs_admin_user}}' AS lower_name, '{{gogs_admin_user}}' AS name, '{{admin_email}}' AS email, '${PASSWORD}' as passwd, '${SALT}' as salt, 1 AS is_active, 1 AS is_admin) AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM user WHERE name = '{{gogs_admin_user}}'
+) LIMIT 1;
+EOF
